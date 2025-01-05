87b1198aad88474ad31955671867923deb6c6145
kiryls <kiryl92@gmail.com>
2025-01-05 03:15:16 +0100

    (adding) logging & assertions

---
+++ engine/src/core/logger.c
@@ -0,0 +1,44 @@
+#include "logger.h"
+#include "asserts.h"
+
+// #todo temporary
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+b8 initialize_logging() {
+    // #todo create log file
+    return TRUE;
+}
+void shutdown_logging() {
+    // #todo cleanup logging/write queued entries
+}
+
+void log_output(log_level level, const char* message, ...) {
+    const char* level_strings[6] = {"[FATAL]: ", "[ERROR]: ", "[WARN]:  ", "[INFO]:  ", "[DEBUG]: ", "[TRACE]: "};
+    // b8 is_error = level < 2;  // #note can also do level <= LOG_LEVEL_ERROR;
+
+    // Technically imposes a 32k character limit on a single log entry, but...
+    // DON'T DO THAT! (bad practice?)
+    char out_message[32000];
+    memset(out_message, 0, sizeof(out_message));
+
+    // Format original message.
+    // #note Oddly enough, MS's headers override the GCC/CLANG va_list type with a "typedef char* va_list" in some
+    // cases, and as a result throws a strange error here. The workaround for now is to just use __builtin_va_list,
+    // which is the type GCC/CLANG's va_start expects.
+    __builtin_va_list arg_ptr;
+    va_start(arg_ptr, message);
+    vsnprintf(out_message, 32000, message, arg_ptr);
+    va_end(arg_ptr);
+
+    char result[32000];
+    sprintf(result, "%s%s\n", level_strings[level], out_message);
+
+    // #todo platform-specific output
+    printf("%s", result);
+}
+
+void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line) {
+    log_output(LOG_LEVEL_FATAL, "Assertion Failure: %s, message: '%s', in file: %s, line: %d\n", expression, message, file, line);
+}
